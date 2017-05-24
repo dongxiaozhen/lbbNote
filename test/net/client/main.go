f39919473b1404f552eeb72981dea29dc40d5512
libinbin <libinbin@17paipai.cn>
2017-05-24 11:31:15 +0800

    update net main

--- test/net/client/main.go
+++ test/net/client/main.go
@@ -1,203 +1,26 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
-	"net"
 	"os"
 	"os/signal"
-	"sync"
 	"syscall"
+	"test/net/common"
 	"time"
 )
-
-type Transport struct {
-	con        *net.TCPConn
-	closeRead  bool
-	closeWrite bool
-	readChan   chan []byte
-	writeChan  chan []byte
-}
-
-func NewTransport(con *net.TCPConn) *Transport {
-	return &Transport{con: con, readChan: make(chan []byte, 10), writeChan: make(chan []byte, 10)}
-}
-func (t *Transport) CloseRead() {
-	fmt.Println("-->transport CloseRead()")
-	if t.closeRead {
-		fmt.Println("-->transport CloseRead() has close read")
-		return
-	}
-	t.con.CloseRead()
-	fmt.Println("-->transport CloseRead() close read")
-}
-
-func (t *Transport) CloseWrite() {
-	fmt.Println("--->Transport CloseWrite()")
-	if t.closeWrite {
-		fmt.Println("--->Transport CloseWrite() has close write")
-		return
-	}
-	t.con.CloseWrite()
-	close(t.writeChan)
-	fmt.Println("--->Transport CloseWrite() close write")
-}
-
-func (t *Transport) ReadData() []byte {
-	if t.closeRead {
-		return nil
-	}
-	return <-t.readChan
-}
-func (t *Transport) WriteData(data []byte) {
-	if t.closeWrite {
-		return
-	}
-	t.writeChan <- data
-}
-
-func (t *Transport) beginWork() {
-	go t.beginToRead()
-	go t.beginToWrite()
-}
-func (t *Transport) beginToRead() {
-	fmt.Println("-->transport read begin")
-	defer func() {
-		t.closeRead = true
-		close(t.readChan)
-		fmt.Println("-->transport read end")
-	}()
-	for {
-		buf := make([]byte, 30)
-		_, err := t.con.Read(buf)
-		if err == io.EOF {
-			fmt.Println("-->transport read err io.EOF")
-			return
-		}
-		t.readChan <- buf
-	}
-}
-
-func (t *Transport) beginToWrite() {
-	fmt.Println("--->transport write begin")
-	defer func() {
-		t.closeWrite = true
-		fmt.Println("--->transport write over")
-	}()
-	for buf := range t.writeChan {
-		n, err := t.con.Write(buf)
-		if err != nil || n != len(buf) {
-			fmt.Println("--->transport write err %#v", err)
-			return
-		}
-	}
-}
-
-type TClient struct {
-	close     bool
-	addr      string
-	wg        sync.WaitGroup
-	pf        Protocol
-	transport *Transport
-}
-
-type Protocol interface {
-	OnTransportMade(t *Transport)
-	OnTransportData(t *Transport, data []byte)
-	OnTransportLost(t *Transport)
-}
-
-func NewTClient(addr string, pf Protocol) *TClient {
-	t := &TClient{addr: addr, pf: pf}
-	t.connect()
-	return t
-}
-
-func (p *TClient) Close() {
-	p.close = true
-	p.wg.Wait()
-}
-
-func (p *TClient) connect() error {
-	conn, err := net.Dial("tcp", p.addr)
-	if err != nil {
-		fmt.Println("dial err")
-		return err
-	}
-	con, ok := conn.(*net.TCPConn)
-	if !ok {
-		fmt.Println("conver err")
-		return errors.New("conv err")
-	}
-	go p.handlerConnect(con)
-	return nil
-}
-func (p *TClient) handlerConnect(con *net.TCPConn) {
-	defer func() {
-		fmt.Println("reconnect")
-		p.recon()
-	}()
-
-	p.wg.Add(1)
-	defer p.wg.Done()
-
-	p.transport = NewTransport(con)
-	p.transport.beginWork()
-	p.pf.OnTransportMade(p.transport)
-
-	defer func() {
-		fmt.Println("send closeWrite()")
-		p.transport.CloseWrite()
-	}()
-
-	defer p.pf.OnTransportLost(p.transport)
-	// close read
-	defer func() {
-		fmt.Println("send closeRead()")
-		p.transport.CloseRead()
-	}()
-
-	p.handlerData()
-}
-
-func (p *TClient) recon() {
-	if p.close {
-		return
-	}
-	for !p.close {
-		err := p.connect()
-		if err != nil {
-			time.Sleep(5 * time.Second)
-		}
-	}
-}
-
-func (p *TClient) handlerData() {
-	defer func() {
-		fmt.Println("tclient handlerData over")
-	}()
-
-	for {
-		if s := p.transport.ReadData(); s == nil {
-			fmt.Println("client handlerData return")
-			return
-		} else {
-			p.pf.OnTransportData(p.transport, s)
-		}
-	}
-}
 
 type Hello struct {
 }
 
-func (h *Hello) OnTransportMade(t *Transport) {
+func (h *Hello) OnTransportMade(t *common.Transport) {
 	fmt.Println("connect mad")
 }
-func (h *Hello) OnTransportData(t *Transport, data []byte) {
+func (h *Hello) OnTransportData(t *common.Transport, data []byte) {
 	fmt.Println("connect data", string(data))
+	time.Sleep(5 * time.Second)
+	t.WriteData([]byte(string(data) + "client"))
 }
-func (h *Hello) OnTransportLost(t *Transport) {
+func (h *Hello) OnTransportLost(t *common.Transport) {
 	fmt.Println("connect lost")
 }
 
@@ -207,7 +30,7 @@ func main() {
 	signal.Notify(closeChan, syscall.SIGTERM)
 
 	hello := &Hello{}
-	t := NewTClient("127.0.0.01:9099", hello)
+	t := common.NewTClient("127.0.0.01:9099", hello)
 
 	<-closeChan
 	t.Close()
--- test/net/server/main.go
+++ test/net/server/main.go
@@ -3,62 +3,144 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"test/net/common"
 	"time"
 )
 
-func main() {
-	fmt.Println("vim-go")
-	ld, err := net.ResolveTCPAddr("tcp", ":8081")
-	l, err := net.ListenTCP("tcp", ld)
+type TServer struct {
+	addr string
+	pf   common.Protocol
+	l    *net.TCPListener
+	wg   sync.WaitGroup
+	mp   map[*common.Transport]struct{}
+}
+
+func NewTServer(addr string, pf common.Protocol) *TServer {
+	t := &TServer{addr: addr, pf: pf, mp: make(map[*common.Transport]struct{})}
+	t.listern()
+	return t
+}
+
+func (p *TServer) Close() {
+	// close listern
+	p.l.Close()
+
+	p.closeRead()
+	p.wg.Wait() // wait read close
+
+	// wait work group
+
+	p.closeWrite()
+
+	p.calllost()
+}
+
+func (p *TServer) calllost() {
+	fmt.Println("TServer call OnTransportLost")
+	for t := range p.mp {
+		p.pf.OnTransportLost(t)
+	}
+}
+
+func (p *TServer) closeRead() {
+	fmt.Println("TServer close read")
+	for t := range p.mp {
+		t.CloseRead()
+	}
+}
+
+func (p *TServer) closeWrite() {
+	fmt.Println("TServer close write")
+	for t := range p.mp {
+		t.CloseWrite()
+	}
+}
+
+func (p *TServer) listern() {
+	ld, err := net.ResolveTCPAddr("tcp", p.addr)
 	if err != nil {
 		fmt.Println("%#v", err)
 		return
 	}
+	p.l, err = net.ListenTCP("tcp", ld)
+	if err != nil {
+		fmt.Println("%#v", err)
+		return
+	}
+	go p.accept()
+}
+
+func (p *TServer) accept() {
+	fmt.Println("TServer accept begin")
+	defer fmt.Println("TServer accept over")
+
 	for {
-		con, err := l.AcceptTCP()
+		con, err := p.l.AcceptTCP()
 		if err != nil {
 			fmt.Println("accept err %#v", err)
 			return
 		}
 		con.SetReadBuffer(44)
 		con.SetNoDelay(true)
-		go handler(con)
+		t := common.NewTransport(con)
+		go p.handler(t)
 	}
 }
 
-func handler(conn *net.TCPConn) {
-	go beginToRead(conn)
-	go beginToWrite(conn)
-	// go closeRead(conn)
+func (p *TServer) handler(t *common.Transport) {
+	fmt.Println("TServer handler begin")
+	defer fmt.Println("TServer handler over")
+
+	p.wg.Add(1)
+	defer p.wg.Done()
+
+	p.pf.OnTransportMade(t)
+	defer func() {
+		if t.Close() {
+			p.pf.OnTransportLost(t)
+			delete(p.mp, t)
+			fmt.Println("auto close: call OnTransportLost and delete transport from mp")
+		}
+	}()
+
+	for {
+		s := t.ReadData()
+		if s == nil {
+			return
+		}
+		p.pf.OnTransportData(t, s)
+	}
 }
 
-func closeRead(conn *net.TCPConn) {
-	time.Sleep(10 * time.Second)
-	conn.CloseRead()
+type Hello struct {
 }
-func beginToRead(conn *net.TCPConn) {
-	fmt.Println("-->read begin")
-	buf := make([]byte, 30)
-	for {
-		n, err := conn.Read(buf)
-		if err != nil {
-			fmt.Println("read err %#v", err)
-			return
-		}
-		fmt.Println(string(buf), n)
-	}
-	fmt.Println("-->read end")
+
+func (h *Hello) OnTransportMade(t *common.Transport) {
+	fmt.Println("t made")
+	time.Sleep(5 * time.Second)
+	t.WriteData([]byte("libinbin"))
 }
-func beginToWrite(conn *net.TCPConn) {
-	fmt.Println("--->write begin")
-	for {
-		n, err := conn.Write([]byte("aaaa"))
-		if err != nil {
-			fmt.Println("write %#v", err)
-			return
-		}
-		fmt.Println(n)
-		time.Sleep(10 * time.Second)
-	}
-	fmt.Println("--->write end")
+func (h *Hello) OnTransportLost(t *common.Transport) {
+	fmt.Println("t lost")
+}
+func (h *Hello) OnTransportData(t *common.Transport, data []byte) {
+	fmt.Println("process data", string(data))
+	time.Sleep(5 * time.Second)
+	t.WriteData(data)
+}
+func main() {
+	fmt.Println("vim-go")
+	closeChan := make(chan os.Signal, 1)
+	signal.Notify(closeChan, syscall.SIGTERM)
+
+	hello := &Hello{}
+	s := NewTServer(":9099", hello)
+	<-closeChan
+	s.Close()
+	time.Sleep(3 * time.Second)
+
 }
