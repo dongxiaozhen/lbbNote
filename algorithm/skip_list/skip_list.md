e6287ad1d7333cd7368e58f487f6d4d34108874d
libinbin <libinbin@17paipai.cn>
2020-02-28 16:29:47 +0800

    algo

---
+++ algorithm/skip_list/skip_list.md
@@ -0,0 +1,42 @@
+跳表
+  核心思想，“空间来换取时间"
+
+(1) 由很多层结构组成
+(2) 每一层都是一个有序的链表
+(3) 最底层(Level 0)的链表包含所有元素
+(4) 如果一个元素出现在 Level i 的链表中，则它在 Level i 之下的链表也都会出现。
+(5) 每个节点包含两个指针，一个指向同一链表中的下一个元素，一个指向下面一层的元素。
+(6 )插入元素的时候，元素所占有的层数完全是随机的 
+
+# 文章 
+  https://www.cnblogs.com/a8457013/p/8251967.html
+
+# 代码
+  github.com/ryszard/goskiplist/skiplist
+
+
+# skiplist
+type SkipList struct {
+    lessThan func(l, r interface{}) bool
+    // header 节点的key,value不使用，每次查询都是从header.forward开始
+    header   *node  
+    footer   *node
+    length   int
+    MaxLevel int
+} 
+
+# 节点，
+type node struct {
+    // forward 指向后一个节点，foward的长度就是该节点的深度,深度0包含所有的节点
+    forward    []*node
+    backward   *node
+    key, value interface{}
+}
+
+# skiplist 的操作和list 一个思想，修改操作节点(新增节点和删除节点)的前后节点的指针, 不一样在 跳表的指向后一个节点的数据结构是 slice, 
+
+
+
+#skiplist.go
+    如果一个元素出现在 Level i 的链表中，则它在 Level i 之下的链表也都会出现。所以，找到level层的节点后，就往level-1层继续查找
+
---
+++ algorithm/skip_list/skiplist.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+const LevelMax = 4
+
+type Element struct {
+	nexts []*Element
+	key   int
+	value int
+}
+
+type skipList struct {
+	front  Element
+	length int
+}
+
+func newSkipList() *skipList {
+	re := &skipList{}
+	re.front.nexts = make([]*Element, LevelMax)
+	return re
+}
+
+func (list *skipList) list() {
+
+	for level := LevelMax - 1; level >= 0; level-- {
+		fmt.Println("----------level", level)
+
+		next := list.front.nexts[level]
+		for next != nil {
+			fmt.Println("value info", next.key, next.value, len(next.nexts))
+			next = next.nexts[level]
+		}
+	}
+}
+
+func (list *skipList) set(key, value int) {
+
+	ppv := list.getPrev(key)
+	if element := ppv[0].nexts[0]; element != nil {
+		element.value = value
+	}
+
+	level := rand.Intn(LevelMax + 1)
+	fmt.Println("random level", level)
+	if level == 0 {
+		level = 1
+	}
+	newElement := &Element{
+		nexts: make([]*Element, level),
+		key:   key,
+		value: value,
+	}
+
+	for i := range newElement.nexts {
+		newElement.nexts[i] = ppv[i].nexts[i]
+		ppv[i].nexts[i] = newElement
+	}
+}
+
+func (list *skipList) get(key int) int {
+
+	prev := &list.front
+	var next *Element
+	for level := LevelMax - 1; level >= 0; level-- {
+
+		next = prev.nexts[level]
+		fmt.Println("---------- get level", level, next)
+		for next != nil && key >= next.key {
+			prev = next
+			next = next.nexts[level]
+		}
+		fmt.Println("---------- get level over", level, prev)
+	}
+
+	if prev != nil && prev.key == key {
+		return prev.value
+	}
+	return 0
+}
+
+func (list *skipList) del(key int) {
+}
+
+func (list *skipList) getPrev(key int) []*Element {
+	ret := make([]*Element, LevelMax)
+
+	prev := &list.front
+	for level := LevelMax - 1; level >= 0; level-- {
+		next := prev.nexts[level]
+		for next != nil && (key >= next.key) {
+			prev = next
+			next = next.nexts[level]
+		}
+
+		ret[level] = prev
+	}
+	fmt.Println("getPrev", ret)
+	return ret
+}
+
+func main() {
+	fmt.Println("vim-go")
+
+	list := newSkipList()
+	list.set(1, 1)
+	list.set(2, 2)
+	list.set(3, 3)
+	list.set(4, 4)
+
+	list.set(14, 14)
+	list.set(10, 10)
+	list.set(44, 44)
+	list.set(24, 24)
+
+	list.list()
+
+	fmt.Println("get ret", list.get(10))
+	fmt.Println("get ret", list.get(9))
+}
